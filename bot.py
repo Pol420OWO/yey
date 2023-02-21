df997bed366df6005f02b76305d661c2216a0812
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-21 20:10:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,11 +14,11 @@ g = Github(user,password)
 repo = g.get_user().get_repo('yey')
 
 file_list = [
-    'C:\Users\polmernie\OneDrive - Centre d'Estudis Monlau\Desktop',
+  'C:\Users\polmernie\OneDrive - Centre d'Estudis Monlau\Desktop',
 ]
 
 file_names = [
-    'pene.bat',
+  'pene.bat',
 ]
 commit_message = 'python update 2'
 master_ref = repo.get_git_ref('heads/master')
@@ -26,12 +26,12 @@ master_sha = master_ref.object.sha
 base_tree = repo.get_git_tree(master_sha)
 element_list = list()
 for i, entry in enumerate(file_list):
-    with open(entry) as input_file:
-        data = input_file.read()
-    if entry.endswith('.png'):
-        data = base64.b64encode(data)
-    element = InputGitTreeElement(file_names[i], '100644', 'blob', data)
-    element_list.append(element)
+  with open(entry) as input_file:
+    data = input_file.read()
+  if entry.endswith('.png'):
+    data = base64.b64encode(data)
+  element = InputGitTreeElement(file_names[i], '100644', 'blob', data)
+  element_list.append(element)
 tree = repo.create_git_tree(element_list, base_tree)
 parent = repo.get_git_commit(master_sha)
 commit = repo.create_git_commit(commit_message, tree, [parent])
