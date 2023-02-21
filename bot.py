8b8b1ef82e4aa45ce8888878324673039e7c3c46
Pol420OWO <118475266+Pol420OWO@users.noreply.github.com>
2023-02-21 20:10:40 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,11 +14,11 @@ g = Github(user,password)
 repo = g.get_user().get_repo('yey')
 
 file_list = [
-  'C:\Users\polmernie\OneDrive - Centre d'Estudis Monlau\Desktop',
+  'C:\Users\polmernie\OneDrive - Centre d'Estudis Monlau\Desktop'
 ]
 
 file_names = [
-  'pene.bat',
+  'pene.bat'
 ]
 commit_message = 'python update 2'
 master_ref = repo.get_git_ref('heads/master')
